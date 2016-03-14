fbcd9f6d03927f913097d0fd876d2af32507d28f
yasinzor <https://github.com/yasinzor>
2016-03-14 22:52:30 +0200

    java projesi 2. sınıf

---
+++ JavaApplication1.java
@@ -0,0 +1,14 @@
+
+package javaapplication1;
+import java.sql.*;
+
+public class JavaApplication1 {
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=convertToNull [root on Default schema]";
+   
+   
+    public static void main(String[] args) {
+     
+    }
+    
+}
